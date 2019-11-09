81444931da8f88fa4bf58ca83b9b29fff7cf1aa7
Sheng Yu <heavy.sheng.yu@gmail.com>
2019-11-09 12:43:17 +0800

    change port 8082

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ var moment = require("moment");
 
 app.use('/', express.static(__dirname + '/www'));
 
-server.listen(process.env.PORT || 3000);//publish to heroku
+server.listen(process.env.PORT || 8082);//publish to heroku
 
-console.log("SERVER IS RUNNING ON", process.env.PORT || 3000)
+console.log("SERVER IS RUNNING ON", process.env.PORT || 8082)
